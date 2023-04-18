4557dd582f587dd160c97dd1b97dd03c685699a3
ʕ̡̢̡ʘ̅͟͜͡ʘ̲̅ʔ̢̡̢..(｡•ˇ‸ˇ•｡) …  . . . . ✧ʕ̢̣̣̣̣̩̩̩̩·͡˔·ོɁ̡̣̣̣̣̩̩̩̩✧ <qxh@mail.com>
2023-04-18 10:14:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Store
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/ssray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -24,6 +24,6 @@ done
 # start
 tor &
 
-/xray -config /xray.json &
+/ssray -config /ssray.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
